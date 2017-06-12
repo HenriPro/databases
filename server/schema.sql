478e330649e3253677ee1c1ee03cbab65a35e8a6
Hack Reactor Student <student@hackreactor.com>
2017-06-12 16:29:01 -0700

    mysql database created

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,32 @@
 CREATE DATABASE chat;
 
-USE chat;
+USE chat
+
+CREATE TABLE rooms (
+  id INT(11) PRIMARY KEY NOT NULL,
+  roomname VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users (
+  id INT(11) PRIMARY KEY NOT NULL,
+  username VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE rooms_users (
+  roomID INT(11) NOT NULL,
+  userID INT(11) NOT NULL,
+  CONSTRAINT PK_RoomsUsers PRIMARY KEY(roomID, userID),
+  FOREIGN KEY (roomID) REFERENCES rooms (id),
+  FOREIGN KEY (userID) REFERENCES users (id) 
+);
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT(11) PRIMARY KEY NOT NULL,
+  userID INT(11),
+  text VARCHAR(255),
+  roomID INT(11),
+  FOREIGN KEY (userID) REFERENCES users (id),
+  FOREIGN KEY (roomID) REFERENCES rooms (id) 
 );
 
 /* Create other tables and define schemas for them here! */
