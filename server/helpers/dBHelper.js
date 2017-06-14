6c7f209444a60cde790b489ffe92828596c3d9ed
Hack Reactor Student <student@hackreactor.com>
2017-06-13 20:03:59 -0700

    posting messages almost works

--- server/helpers/dBHelper.js
+++ server/helpers/dBHelper.js
@@ -2,13 +2,13 @@ var mysql = require('mysql');
 const Sequelize = require('sequelize');
 const db = new Sequelize('chat', 'root', 'plantlife');
 
-exports.dbConnection = () => { 
-  return mysql.createConnection({
-    user: 'root',
-    password: 'plantlife',
-    database: 'chat'
-  });
-};
+// exports.dbConnection = () => { 
+//   return mysql.createConnection({
+//     user: 'root',
+//     password: 'plantlife',
+//     database: 'chat'
+//   });
+// };
 
 var Users = db.define('users', {
   username: Sequelize.STRING
@@ -32,83 +32,76 @@ Users.hasMany(Messages, {foreignKey: 'userID'});
 Messages.belongsTo(Rooms, {foreignKey: 'roomID', targetKey: 'id'});
 Rooms.hasMany(Messages, {foreignKey: 'roomID'});
 
-exports.buildField = (obj, tableName, fieldName, cb) => {
+// exports.buildField = (obj, tableName, fieldName, cb) => {
 
-  var connection = exports.dbConnection();
-  connection.connect();
-  
-  var insertString = `INSERT INTO ${tableName} (${fieldName}) VALUES ("${obj[tableName]}");`;
-  var insertArgs = [];
-  console.log('INSERTING ', insertString);
-  connection.query(insertString, insertArgs, function(err, results) {
-    if (err) {
-      console.log('Error inserting into ', tableName, fieldName, err);
-      cb(err);
-    } else {
-      console.log('Successfully inserted into ', tableName, fieldName, results);
-      cb(results);
-    }
-    connection.end();
-  });
-
-};
-
-exports.buildMessage = (obj, cb) => {
-  
-  Messages.create( {userID: ''})
-};
- 
-
-// exports.buildMessage = (obj, cb) => {
-//   // console.log('HEREEERERE', obj.username, obj.roomname, obj.text);
 //   var connection = exports.dbConnection();
 //   connection.connect();
-
-//   // SELECT id from users WHERE username ="${obj.username}"
-//   exports.getIdFromTable('username', obj.username, 'users', (userId) => {
-//     exports.getIdFromTable('roomname', obj.roomname, 'rooms', (roomId) => {
-//       var insertString = `INSERT INTO messages (userID, text, roomID) VALUES (${userId}, "${obj.text}", ${roomId});`;
-//       var insertArgs = [];
-//       console.log('INSERTING ', insertString);
-//       connection.query(insertString, insertArgs, function(err, results) {
-//         if (err) {
-//           console.log('Error inserting into messages', err);
-//           cb(err);
-//         } else {
-//           console.log('Successfully inserted into messages', results);
-//           cb(results);
-//         }
-//         connection.end();
-//       });
-//     });
+  
+//   var insertString = `INSERT INTO ${tableName} (${fieldName}) VALUES ("${obj[tableName]}");`;
+//   var insertArgs = [];
+//   console.log('INSERTING ', insertString);
+//   connection.query(insertString, insertArgs, function(err, results) {
+//     if (err) {
+//       console.log('Error inserting into ', tableName, fieldName, err);
+//       cb(err);
+//     } else {
+//       console.log('Successfully inserted into ', tableName, fieldName, results);
+//       cb(results);
+//     }
+//     connection.end();
 //   });
-  
-// };  
 
-//roomname, searchvalue, rooms
-//username, searchvalue, users
-exports.getIdFromTable = (key, searchValue, tableName, cb) => {
-  // console.log('Inside getIdFromTable with parameters: ', key, searchValue, tableName);
-  var connection = exports.dbConnection();
-  connection.connect();
+// };
   
-  var querryString = `SELECT ID from ${tableName} WHERE ${key} = (${connection.escape(searchValue)});`;
-  var insertArgs = [];
-  console.log('SELECTING ', querryString);
-  connection.query(querryString, insertArgs, function(err, results) {
-    if (err) {
-      console.log(`Error SELECTING into ${tableName}`, err);
-      cb(err);
-    } else {
-      console.log('Successfully SELECTED', results);
-      cb(results[0].ID);
-    }
-    connection.end();
+
+exports.buildMessage = (obj, cb) => {
+
+  var message = {text: obj.text};
+  db.sync()
+  .then( () => {  
+    return exports.getUserIdPromise(obj.username);
+  })
+  .then( (user) => {
+    console.log('GOT USER: ', user.id);
+    message.userID = user.id;
+  })
+  .then( () => {
+    return exports.getRoomIdPromise(obj.roomname);
+  })
+  .then( (room) => {
+    console.log('GOT ROOM: ', room.id);
+    message.roomID = room.id;
+  })
+  .then( () => {
+    return Messages.create(message);
+  })
+  .catch(function(err) {
+    // Handle any error in the chain
+    console.error(err);
+    db.close();
+  })
+  .then( () => {
+    cb();
+    db.close();
   });
+
+
 };
 
-exports.getAllMessages = (req, cb) => {
+exports.getUserIdPromise = (name) => {
+  return Users.find( { where: [ {username: name} ]});
+};
 
+exports.getRoomIdPromise = (room) => {
+  return Rooms.find( { where: [ {roomname: room} ]});
+};
+
+
+var counter = 0;
+
+exports.getAllMessages = (req, cb) => {
+  counter++;
+// console.log('COUNTER: ', counter);
   db.sync()
     .then( () => {
       
@@ -127,9 +120,12 @@ exports.getAllMessages = (req, cb) => {
       });
       console.log('Sequelizeed messages..................', messagesTransformed);
       
+      // db.close(); //WHY DOES IT DO THIS
+    
       cb(messagesTransformed);
     })
     .catch( (err) => {
+      db.close();
       console.log('ERR when sequelizing messages......', err);
     });
 };
@@ -137,8 +133,5 @@ exports.getAllMessages = (req, cb) => {
 
 
 
-// exports.getAllMessages({}, (messages)=> {
-//   console.log('***** MESSAGES: ', messages);
-// });
-
+// exports.buildMessage({username: 'batman', roomname: 'lobby', text: 'well hello'}, () => {console.log('success')} );
 
